a12ca861db42d2ea72fc55222398247672e07bd7
guilhermecastanheira <guilhermecastanheira223.gc@gmail.com>
2020-08-22 17:37:16 -0300

    exemplo digital innovation one

---
+++ Controllers/InfectadoController.cs
@@ -0,0 +1,40 @@
+using MongoDB.Driver;
+using labDIO.Data;
+using labDIO.Models;
+using Microsoft.AspNetCore.Mvc;
+using labDIO.Collections;
+
+namespace labDIO.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class InfectadoController : ControllerBase
+    {
+        Data.MongoDB _mongoDB;
+        IMongoCollection<Infectado> _infectadosCollection;
+
+        public InfectadoController(Data.MongoDB mongoDB)
+        {
+            _mongoDB = mongoDB;
+            _infectadosCollection = _mongoDB.DB.GetCollection<Infectado>(typeof(Infectado).Name.ToLower());
+        }
+
+        [HttpPost]
+        public ActionResult SalvarInfectado([FromBody] InfectadosDTO dto)
+        {
+            var infectado = new Infectado(dto.DataNascimento, dto.Sexo, dto.Latitude, dto.Longitude);
+
+            _infectadosCollection.InsertOne(infectado);
+            
+            return StatusCode(201, "Infectado adicionado com sucesso");
+        }
+
+        [HttpGet]
+        public ActionResult ObterInfectados()
+        {
+            var infectados = _infectadosCollection.Find(Builders<Infectado>.Filter.Empty).ToList();
+            
+            return Ok(infectados);
+        }
+    }
+}
---
+++ Models/InfectadosDTO.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace labDIO.Models
+{
+    public class InfectadosDTO
+    {
+        public DateTime DataNascimento { get; set; }
+        public string Sexo { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
